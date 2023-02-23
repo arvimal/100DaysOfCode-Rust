c1418e0ac9c0c73e94353979fe8e22eb19e71292
Vimal A R <arvimal81@gmail.com>
2023-02-23 20:45:05 +0100

    001 Day of 100DaysofCode in Rust

---
+++ 001/01-hello_100DaysofCode.rs
@@ -0,0 +1,10 @@
+// Rust Program
+//
+// 1. All rust programs has to have a main() function
+// 2. println! is a built-in macro
+// 3. All macros are called by appending `!` to the macro name.
+// 4. Read more about Macros and Functions at https://doc.rust-lang.org/book/ch19-06-macros.html
+// 5. Build the binary executable with `rustc <file-name>.rs`
+fn main() {
+    println!("Hello 100DaysofCode-Rust!")
+}
